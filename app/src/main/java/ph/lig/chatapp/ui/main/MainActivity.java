df37c0792d3d1327e0dfcc7f595ab723cec26037
Virgil John Lomocso <virgil@fullscale.io>
2020-07-20 04:37:45 +0800

    implement unfocus for text boxes

--- app/src/main/java/ph/lig/chatapp/ui/main/MainActivity.java
+++ app/src/main/java/ph/lig/chatapp/ui/main/MainActivity.java
@@ -1,7 +1,12 @@
 package ph.lig.chatapp.ui.main;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -56,6 +61,23 @@ public class MainActivity extends AppCompatActivity implements NavController.OnD
         } else {
             mViewModel.setTitleBarShown(true);
         }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 
     public void onClickLogOut(View view) {
